0b3adb602bb8c5474e7a7b59e6c52cffa7145698
aniket965 <aniket965.as@gmail.com>
2017-09-02 11:10:20 +0530

    added Forms and lInked

---
+++ src/Components/Home/Form/index.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import { TextField, } from 'office-ui-fabric-react/lib/TextField';
+import {
+  ComboBox,
+  IComboBoxOption
+} from 'office-ui-fabric-react/lib/ComboBox';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+
+import { Label } from 'office-ui-fabric-react/lib/Label';
+
+var years =
+  [
+    { key: '1', text: 'First Year' },
+    { key: '2', text: 'Second Year' },
+    { key: '3', text: 'Third Year' },
+    { key: '4', text: 'Fourth Year' },
+
+  ];
+
+class Form extends Component {
+  render() {
+    return (
+      <div>
+        <TextField placeholder='firstname lastname' label="Name" required={true} ariaLabel='Please enter text here' />
+        <TextField placeholder='user@example.com' label="Email" required={true} ariaLabel='Please enter text here' />
+        <TextField  label="Phone Number" required={true} ariaLabel='Please enter text here' addonString="+91" />
+        <TextField placeholder='mugd' label="Github Handle" required={false} ariaLabel='Please enter text here' addonString="https://github.com/" />
+        <ComboBox
+          defaultSelectedKey='1'
+          label='Year'
+          id='Basicdrop1'
+          ariaLabel='Basic ComboBox example'
+          allowFreeform={false}
+          autoComplete='on'
+          options={years}
+        />
+       
+        <br/>
+        <br/>
+
+      
+      </div>
+    )
+  }
+}
+
+
+
+export default Form
--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -3,10 +3,23 @@ import { Image } from 'office-ui-fabric-react/lib/Image';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import { Label } from 'office-ui-fabric-react/lib/Label';
 import ButtonSection from './ButtonSection'
+import {
+  CommandButton, PrimaryButton,
+  IButtonProps
+} from 'office-ui-fabric-react/lib/Button';
 import Heading from './../Heading'
 import Content from './Content'
-import EventCard from './EventCard'
+import Form from './Form'
+import About from './About'
 var axios = require('axios');
+var years =
+  [
+    { key: '1', text: 'First Year' },
+    { key: '2', text: 'Second Year' },
+    { key: '3', text: 'Third Year' },
+    { key: '4', text: 'Fourth Year' },
+
+  ];
 
 class Home extends Component {
   constructor(props) {
@@ -14,6 +27,7 @@ class Home extends Component {
     this.state = {
       bingBackUrl: "",
       width: window.innerWidth,
+      isFomSelected: false
     }
   }
   componentWillMount() {
@@ -39,11 +53,11 @@ class Home extends Component {
         <div>
           <Image src={this.state.bingBackUrl} className="back" />
           <Grid fluid>
-          <Row center="xs">
+            <Row center="xs">
               <Col xs={12} md={8} lg={8}>
                 <Heading />
               </Col>
-              </Row>
+            </Row>
 
             <Content />
             <br />
@@ -54,29 +68,80 @@ class Home extends Component {
     } else {
       return (
         <div>
-        <Image src={`https://scontent.fdel4-1.fna.fbcdn.net/v/t31.0-8/12079814_920071178087110_6324831873201700019_o.jpg?oh=dadc433deda4c3b385a66632b7f3157b&oe=5A5AA31B`} className="back1" />
-          
+          <Image src={`https://scontent.fdel4-1.fna.fbcdn.net/v/t31.0-8/12079814_920071178087110_6324831873201700019_o.jpg?oh=dadc433deda4c3b385a66632b7f3157b&oe=5A5AA31B`} className="back1" />
+
           <Grid>
-          
+
             <Row >
-          
-            <Col xs={0} md={8} lg={8}>
-           
-            </Col>
-              <Col xs={0} md={4} lg={4}>
-              <br/>
-              <br/>
-              <br/>
-              <h1 className="ms-font-su ms-fontWeight-semibold headingdesktop">Microsoft Technical Group</h1>
-              <div className="ms-font-xl desktopcontent"> Bvp Microsoft Technical Group is Microsoft student chapter which is managed It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</div>
-              <br/>
-              <br/>
-              <ButtonSection />
+
+              <Col xs={0} sm={8} md={8} lg={8}>
               </Col>
-              </Row>
-         
-       
-     
+              <Col xs={0} md={4} lg={4}>
+                <br />
+
+                <br />
+                <h1 className="ms-font-su ms-fontWeight-semibold headingdesktop">Microsoft Technical Group</h1>
+                {this.state.isFomSelected ? (
+                  <div>
+                    <Form />
+                    <Row >
+
+                      <Col xs={4} md={3} lg={3}>
+                        <PrimaryButton
+                          data-automation-id='test'
+                          checked={false}
+                          text='Back'
+                          sty
+                          onClick={() => this.setState({ isFomSelected: false })}
+                        />
+                      </Col>
+                      <Col xs={4} md={4} lg={4}>
+                        <CommandButton
+                          data-automation-id='test'
+                          iconProps={{ iconName: 'AddFriend' }}
+                          disabled={false}
+                          checked={false}
+                        >
+                          Register
+          </CommandButton>
+                      </Col>
+                    </Row>
+
+                  </div>
+                ) : (
+                    <div>
+                      <About />
+                      <br/>
+                      <br/>
+                      <br/>
+                      <Row >
+
+                        <Col xs={4} md={3} lg={3}>
+                          <PrimaryButton
+                            data-automation-id='test'
+                            checked={false}
+                            text='Join'
+                            onClick={() => this.setState({ isFomSelected: true })}
+                          />
+                        </Col>
+                        <Col xs={4} md={4} lg={4}>
+
+                          <PrimaryButton
+                            data-automation-id='test'
+                            checked={false}
+                            text='Events'
+                            onClick={() => alert('Clicked')}
+                          />
+                        </Col>
+                      </Row>
+
+                    </div>
+                  )}
+
+
+              </Col>
+            </Row>
+
           </Grid>
         </div>
       )
